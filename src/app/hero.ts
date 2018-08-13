9eed23ffbb667cb368485d3880538c5e7ad5c877
Yoko Hirakawa <y@na.nu>
2018-08-13 22:23:51 +1000

    my first Angular project!..kind of works but ugly!

---
+++ src/app/hero.ts
@@ -0,0 +1,17 @@
+export class Hero {
+  id: number;
+  name: string;
+  text: string;
+  favourites_count: number;
+  reply_count: number;
+  retweet_count: number;
+  created_at: number;
+}
+
+	
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,23 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    const heroes = [
+  { id: 1, name: 'hoh', text: 'yay', favourites_count: 1,reply_count: 1,retweet_count: 1,created_at:1 },
+  { id: 1, name: 'hoh',text: 'yay',favourites_count: 1,reply_count: 1,retweet_count: 1,created_at:1 },
+  { id: 1, name: 'hoh',text: 'yay',favourites_count: 1,reply_count: 1,retweet_count: 1,created_at:1 },
+  { id: 1,name: 'hoh',text: 'yay', favourites_count: 1,reply_count: 1,retweet_count: 1,created_at:1 },
+  { id: 1,name: 'hoh', text: 'yay',favourites_count: 1,reply_count: 1,retweet_count: 1,created_at:1 },
+  { id: 1, name: 'hoh',text: 'yay',favourites_count: 1,reply_count: 1,retweet_count: 1,created_at:1 },
+  { id: 1, name: 'hoh',text: 'yay',favourites_count: 1,reply_count: 1,retweet_count: 1,created_at:1 },
+    ];
+    return {heroes};
+  }
+}
+
+
+/*
+Copyright 2017-2018 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
